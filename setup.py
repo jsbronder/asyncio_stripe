6bbb4feb511614bc6519fa048eae85ecffb4bd24
Justin Bronder <jsbronder@cold-front.org>
2017-04-15 13:12:29 -0400

    asyncio_stripe-0.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(paths):
 
 setuptools.setup(
         name='asyncio_stripe',
-        version='0.0.1',
+        version='0.1.0',
         description='Asyncio Stripe API bindings',
         license='BSD',
         long_description=(read('README.rst')),
